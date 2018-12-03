c2d8e4e9a79595d3caf8b36a1b8e6d45081aca12
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2018-12-03 18:08:10 +0330

    fix args

--- searchReplace.sh
+++ searchReplace.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-for file in ./Workspace/VideoVarzesh3/src/hawk/sass/component/** ;do
+for file in $3 ;do
     if [[ -f $file ]] && [[ -w $file ]]; then
-        sed -i -- 's/$1/$2/g' "$file"
+        sed -i -- 's/'$1'/'$2'/g' "$file"
     fi
-    "$file"
 done
