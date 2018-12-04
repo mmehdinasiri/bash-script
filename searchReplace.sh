c0a33ba45bc2e5dd17e326f0a078abce93982d53
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2018-12-04 17:54:40 +0330

    input string

--- searchReplace.sh
+++ searchReplace.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for file in $3 ;do
     if [[ -f $file ]] && [[ -w $file ]]; then
-        sed -i -- 's/'$1'/'$2'/g' "$file"
+        sed -i -- 's/'"$1"'/'"$2"'/g' "$file"
     fi
 done
